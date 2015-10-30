cc87256e9eca74448ca08d0268b7d5f72bd9c950
Man-gel <night_captain@hotmail.com>
2015-10-29 22:11:42 -0600

    Se modifica Contacto.java y activity_contacto.xml, ahora ya se muestra en otra Activity la información del contacto

--- App/src/com/example/contactos/Contacto.java
+++ App/src/com/example/contactos/Contacto.java
@@ -4,13 +4,26 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class Contacto extends Activity {
+public class Contacto extends Activity 
+{
+	private String nombre;
+	private String numero;
+	private Integer idImg;
+	private TextView tvNombre;
+	private TextView tvNumero;
+	private ImageView ivImagen;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_contacto);
+		tvNombre = (TextView)findViewById(R.id.tv_nombre);
+		tvNumero = (TextView)findViewById(R.id.tv_phone);
+		ivImagen = (ImageView)findViewById(R.id.iv_detalleImg);
+		obtenerYajustar();
 	}
 
 	@Override
@@ -30,6 +43,17 @@ public class Contacto extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	private void obtenerYajustar()
+	{
+		Bundle recibido = getIntent().getExtras();
+		nombre = recibido.getString("nombre");
+		numero = recibido.getString("phone");
+		idImg = recibido.getInt("idImagen");
+		tvNombre.setText(nombre);
+		tvNumero.setText(numero);
+		ivImagen.setImageResource(idImg);
 	}
 	
 }
